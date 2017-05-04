7ec7da6f5c25e500aece2368fa75bd156c0d05fd
FILESTRING\toan.tran <toan.tran@filestring.com>
2017-05-04 12:02:48 +0700

    add sample

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HelloUser from './HelloUser';
+import Prop1 from './Prop1'
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
+// function formatName(user) {
+//     return user.firstName + ' ' + user.lastName;
+// }
 
-const user = {
-    firstName: 'toan',
-    lastName: 'tran'
-};
+// const user = {
+//     firstName: 'toan',
+//     lastName: 'tran'
+// };
 
-const element = (
-    <h1>Hello, {formatName(user)}</h1>
-);
+// const element = (
+//     <h1>Hello, {formatName(user)}</h1>
+// );
 
+// ReactDOM.render(
+//     element
+//     ,document.getElementById('root')
+// );
+
+
+// ReactDOM.render(
+//     <HelloUser name="Toan Tran" />,
+//     document.getElementById('root')
+// );
+
+
+// test Prop1
 ReactDOM.render(
-    element
-    ,document.getElementById('root')
+    <Prop1 
+        img='https://avatars3.githubusercontent.com/u/11367920?v=3&s=460'
+        name='Toan'
+        username="Tran" />,
+    document.getElementById('root')
 );
