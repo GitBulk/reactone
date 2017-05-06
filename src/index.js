52d978912dc9a5bc061acda1b5bb8229d9fcb727
gitbulk <giacatiensinh@hotmail.com>
2017-05-07 00:57:27 +0700

    update sample

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello world</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 100);
+
 function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 }
 
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)} !!!</h1>;
+    }
+    return <h1>Hello, Stranger</h1>;
+}
+
 const user = {
     firstName: 'toan',
-    lastName: 'tran'
+    lastName: 'tran',
+    avatarUrl: ''
 };
 
 const element = (
-    <h1>Hello, {formatName(user)}</h1>
+    // <h1>Hello, {formatName(user)}</h1>
+    getGreeting(user)
 );
 
-ReactDOM.render(
-    element
-    ,document.getElementById('root')
-);
+const element2 = <div tabIndex="0"></div>
+
+const element3 = <img src={user.avatarUrl} />
+
+const element4 = (
+    <div>
+        <h1>Hello!</h1>
+        <h2>Good to see you here.</h2>
+    </div>
+)
+
+// ReactDOM.render(
+//     element4
+//     ,document.getElementById('root')
+// );
