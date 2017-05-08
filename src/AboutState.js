8636c1aedad15acbd0782e1c453bfc18356fe761
gitbulk <giacatiensinh@hotmail.com>
2017-05-09 00:25:10 +0700

    update state sample, add interval

--- src/AboutState.js
+++ src/AboutState.js
@@ -4,7 +4,23 @@ class Clock extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
     }
 
     render() {
