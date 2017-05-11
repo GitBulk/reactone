f1c1e15f5712b0bcdd55501b48b2cdcc3edcfcb1
FILESTRING\toan.tran <toan.tran@filestring.com>
2017-05-11 17:11:58 +0700

    use share state between 2 TemperatureInput

--- src/Calculator.js
+++ src/Calculator.js
@@ -33,19 +33,21 @@ class TemperatureInput extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            temperature: ''
-        };
+        // this.state = {
+        //     temperature: ''
+        // };
     }
 
     handleChange(event) {
-        this.setState({
-            temperature: event.target.value
-        });
+        // this.setState({
+        //     temperature: event.target.value
+        // });
+        this.props.onTemperatureChange(event.target.value);
     }
 
     render() {
-        const temperature = this.state.temperature;
+        //const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
         const scale = this.props.scale;
         return (
             <div>
@@ -60,10 +62,33 @@ class TemperatureInput extends Component {
 
 class Calculator extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleCelciusChange = this.handleCelciusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {
+            temperature: '',
+            scale: 'c'
+        };
+    }
+    
+    handleCelciusChange(newTemperature) {
+        this.setState({scale: 'c', temperature: newTemperature});
+    }
+
+    handleFahrenheitChange(newTemperature) {
+        this.setState({scale: 'f', temperature: newTemperature});
+    }
+
     render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = (scale === 'f' ? tryConvert(temperature, toCelsius) : temperature);
+        const fahrenheit = (scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature);
+
         return (
             <div>
-                <TemperatureInput scale="c" />
+                <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelciusChange} />
                 <TemperatureInput scale="f" />
             </div>
         );
