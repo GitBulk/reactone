34385834326a9a651b89ae366d509d105aafd5c1
gitbulk <giacatiensinh@hotmail.com>
2017-05-28 19:40:08 +0700

    finish tic tac toe

--- src/tictactoe/Game.js
+++ src/tictactoe/Game.js
@@ -1,27 +1,93 @@
 import React, { Component } from 'react';
 import './Game.css';
 
-class Square extends Component {
+/*class Square extends Component {
 
     render() {
         return (
-            <button className="square">
-                {
-                    // TODO
-                }
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
+}*/
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends Component {
 
-    renderSquare() {
-        return <Square />;
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        };
+    }
+
+    handleClick(i) {
+        // copy the squares array
+        const squares = this.state.squares.slice();
+
+        if (this.calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} />
+        );
+    }
+
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (var i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+
+        return null;
     }
 
     render() {
-        const status = 'Next player X';
+
+        //const status = 'Next player ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -49,8 +115,19 @@ class Board extends Component {
 }
 
 class Game extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true
+        };
+    }
+
     render() {
-        return(
+        return (
             <div className="game">
                 <div className="game-board">
                     <Board />
